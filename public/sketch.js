52ee44f877dcb3f2084d6d922f8305447af37384
ludovicarossi <ludovica4.rossi@mail.polimi.it>
2021-11-17 12:55:26 +0100

    Added broadcast.

--- public/sketch.js
+++ public/sketch.js
@@ -1,17 +1,36 @@
 let clientSocket = io();
 
 clientSocket.on("connect", newConnection);
+clientSocket.on("mouseBroadcast", newBroadcast);
 
 //when someone will connect to the server, everything will be run in the client's computer.
 function newConnection() {
   console.log(clientSocket.id);
 }
 
+function newBroadcast(data) {
+  console.log(data);
+  fill("red");
+  circle(data.x, data.y, 10);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  fill("yellow");
   circle(mouseX, mouseY, 20);
+}
+
+function mouseMoved() {
+  //create an object in which to put x and y variable containg mouse position
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  //I send the object to the server
+  clientSocket.emit("mouse", message);
 }
--- server.js
+++ server.js
@@ -2,20 +2,20 @@ console.log("up and running");
 
 let express = require("express");
 
-//activate it by creating a new variable
+//activate it by creating a new variable.
 let app = express();
 
-//define the port we want to use
+//define the port we want to use.
 let port = 3000;
 
-//create a server variable telling to our express to listen to the port we defined. In this way we have simulated a local host
+//create a server variable telling to our express to listen to the port we defined. In this way we have simulated a local host.
 let server = app.listen(port);
 
 console.log("Server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
 
-//activate the socket library and make it run on the server we have previously activated using express
+//activate the socket library and make it run on the server we have previously activated using express.
 let serverSocket = require("socket.io");
 
 let io = serverSocket(server);
@@ -25,4 +25,11 @@ io.on("connection", newConnection);
 
 function newConnection(newSocket) {
   console.log(newSocket.id);
+  //when the new socket sends to me a message called mouse, I should do something on the server.
+  newSocket.on("mouse", mouseMessage);
+  function mouseMessage(dataReceived) {
+    console.log(dataReceived);
+    //send a message to all the other clients, apart the one that we are using.
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
 }
